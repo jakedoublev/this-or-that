07ec865fc07b9b2588854579352f6e0d013c0e10
jakedoublev <vanvorhisjake@gmail.com>
2024-12-31 13:08:17 -0800

    fix(ci): set base path in vite build for GH pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/this-or-that/", // Set the base path for GitHub Pages deployment
+});
